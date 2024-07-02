3dae3109f336bc384b900936c6d83a3753791717
Hari Babu M <103945712+Hari574@users.noreply.github.com>
2024-07-02 17:29:24 +0530

    tested sample

--- mqtt_publisher.py
+++ mqtt_publisher.py
@@ -1,13 +1,29 @@
 import paho.mqtt.client as mqtt
+import time
 
 broker = "localhost"
-port = "1883"
+port = 1883
 topic = "mqtt/sampletopic"
 
 client = mqtt.Client()
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print(f"Connected to MQTT Broker successfully.")
+    else:
+        print(f"Failed to connect to MQTT Broker.")
+
+client.on_connect = on_connect
+
 client.connect(broker, port, 60)
 
-message = "Hello.. This is sample MQTT"
-client.publish(topic, message)
+value = 0
+while True:
+    value = value + 1
+    message = f"Message {value}"
+    client.loop_start()
+    client.publish(topic, message)
+    client.loop_stop()
+    time.sleep(2)
 
 client.disconnect()
--- mqtt_subscriber.py
+++ mqtt_subscriber.py
@@ -7,10 +7,19 @@ topic = "mqtt/sampletopic"
 def on_message(client, userdata, message):
     print(f"Message received: {message.payload.decode()}")
 
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print(f"Connected to MQTT Broker successfully.")
+        client.subscribe(topic)
+    else:
+        print(f"Failed to connect to MQTT Broker.")
+
 client = mqtt.Client()
+
+client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect(broker, port, 60)
-client.subscribe(topic)
 
 client.loop_forever()
